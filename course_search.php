7547c9414e20a0a89b6a2267f979ba9116fd643f
rustt71749 <60583446+rustt71749@users.noreply.github.com>
2020-09-21 09:55:07 +1200

    df

--- course_search.php
+++ course_search.php
@@ -1,34 +1,14 @@
 <?php include "topbit.php"; 
 
 // if  button pushed
-if(isset($_POST['find_rating']))
+if(isset($_POST['find_course']))
 
 {
 
-// Retrieves rating and sanitises it
-$amount=test_input(mysqli_real_escape_string($dbconnect, $_POST['amount']));
-$stars=test_input(mysqli_real_escape_string($dbconnect, $_POST['stars']));
+// Retrieves course and sanitises it
+$course=test_input(mysqli_real_escape_string($dbconnect, $_POST['course']));
     
-if ($amount=="exactly")
-    
-{
-    $find_sql="SELECT * FROM `L1_DB_assess_TreRus` WHERE `Rating` = $stars
-    ORDER BY `Title` ASC ";
-}
-
-elseif ($amount=="less")
-    
-{
-    $find_sql="SELECT * FROM `L1_DB_assess_TreRus` WHERE `Rating` <= $stars
-    ORDER BY `Title` ASC ";
-}
-    
-else {
-    $find_sql="SELECT * FROM `L1_DB_assess_TreRus` WHERE `Rating` >= $stars
-    ORDER BY `Title` ASC ";
-}
-
-
+$find_sql="SELECT * FROM `L1_DB_assess_TreRus` WHERE `Course` LIKE '%$course%' ORDER BY `course` ASC ";
 $find_query=mysqli_query($dbconnect, $find_sql);
 $find_rs=mysqli_fetch_assoc($find_query);
 $count=mysqli_num_rows($find_query);
@@ -37,7 +17,7 @@ $count=mysqli_num_rows($find_query);
         
 <div class="box main">
             
-    <h1>Rating Search</h1>
+    <h1>Course Search</h1>
             
     <?php
     
@@ -74,7 +54,7 @@ $count=mysqli_num_rows($find_query);
         
         <p>Location: <span class="sub_heading"><?php echo $find_rs['Location']; ?></span></p>
         
-        <p>Course: <span class="sub_heading"><?php echo $find_rs['Genre']; ?></span></p>
+        <p>Course: <span class="sub_heading"><?php echo $find_rs['Course']; ?></span></p>
         
         <p>Vege: <span class="sub_heading"><?php echo $find_rs['Vege']; ?></span></p>
         
